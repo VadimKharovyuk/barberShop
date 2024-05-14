1544d361433e24c6cff2d6c60ceb34493d8950f7
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-05-15 01:53:53 +0300

    add new pic

--- src/main/java/com/example/barbershop/controller/TreatmentController.java
+++ src/main/java/com/example/barbershop/controller/TreatmentController.java
@@ -1,11 +1,14 @@
 package com.example.barbershop.controller;
 
+import com.example.barbershop.model.Treatment;
 import com.example.barbershop.service.TreatmentService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
@@ -15,9 +18,10 @@ public class TreatmentController {
 
 
 
-    @GetMapping("/treatments")
-    public String showAllTreatments(Model model) {
-        model.addAttribute("treatments", treatmentService.getAllTreatments());
-        return "treatments-list"; // Имя представления для отображения списка услуг
+    @GetMapping("/services")
+    public String showServices(Model model) {
+        List<Treatment> services = treatmentService.getAllTreatments();
+        model.addAttribute("services", services);
+        return "services";
     }
 }
